02ef65a0416cfd1013037d8e01bebc518c4da397
Marcelo Anton <marcelo.anton@corp.terra.com.br>
2012-10-17 16:38:51 -0300

    Update to version v0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nose-mongoengine',
-      version="0.1.0",
+      version="0.2.0",
       classifiers=[
         'Environment :: Plugins',
         'Intended Audience :: Developers',
