dbb6d85291be71a2f1e24a9720812d95098ba949
Marcelo Anton <marceloanton@gmail.com>
2012-10-18 00:49:51 -0300

    Improvements in setup.py file.

--- setup.py
+++ setup.py
@@ -1,24 +1,44 @@
 from setuptools import setup, find_packages
 
+DESCRIPTION = """A nose plugin to facilitate the creation of automated
+tests that access Mongo Engine structures."""
+
+f = open("README.rst")
+try:
+    try:
+        LONG_DESCRIPTION = f.read()
+    except:
+        LONG_DESCRIPTION = ""
+finally:
+    f.close()
+
+
+CLASSIFIERS = [
+    'Environment :: Plugins',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Operating System :: OS Independent',
+    'Topic :: Internet :: WWW/HTTP',
+]
+
+
 setup(name='nose-mongoengine',
       version="0.2.0",
-      classifiers=[
-        'Environment :: Plugins',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP'],
+      classifiers=CLASSIFIERS,
       author='Marcelo Anton',
       author_email='marceloanton@gmail.com',
-      description="A nose plugin to facilitate the creation of automated tests that access Mongo Engine structures.",
-      long_description=open("README.md").read(),
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
       url='https://github.com/mbanton/nose-mongoengine/',
+      download_url='https://github.com/mbanton/nose-mongoengine/tarball/master',
       license='BSD-derived',
-      packages=find_packages(),
+      packages=find_packages(exclude=('tests',)),
       install_requires=["nose", "mongoengine"],
       include_package_data=True,
       zip_safe=True,
+      platforms=['any'],
       entry_points = {
         'nose.plugins': ['mongoengine = nose_mongoengine:MongoEnginePlugin'],
       },
 )
+
